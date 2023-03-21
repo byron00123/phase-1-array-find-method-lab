8d1687d09fc372845045f5e94a238f0da1d80017
Byron Omondi <omondibyron25@gmail.com>
2023-03-21 12:26:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(games) {
+    const winningGame = games.find(game => game.result === "W");
+    return winningGame ? winningGame.year : undefined;
+  }
+  
